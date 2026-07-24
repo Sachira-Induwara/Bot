948ddb91e03e14b9017f17f5ebdd55b49dcce812
Sachira-Induwara <sachirainduwara72@gmail.com>
2026-07-24 19:44:16 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botName: "SACHIYA MINI BOT",
+    ownerNumber: "94760579211",
+    prefix: ".",
+    // මෙන්න මෙතනට ඔයාගේ Session ID එක Paste කරන්න:
+    sessionId: process.env.SESSION_ID || "ඔයාගේ_SESSION_ID_එක_මෙතනට_PASTE_කරන්න" 
+};
