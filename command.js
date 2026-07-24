c9f1dd6154b8a33cdfb6f182aeea138fb7fe5585
Sachira-Induwara <sachirainduwara72@gmail.com>
2026-07-24 20:02:05 +0530

    Update command.js

--- command.js
+++ command.js
@@ -1,35 +1,20 @@
-const config = require('./config');
+const fs = require('fs');
+const path = require('path');
 
 const commands = {};
 
-function addCommand(name, desc, category, execute) {
-    commands[name] = {
-        name,
-        desc,
-        category,
-        execute
-    };
+// plugins folder එක ඇතුලේ තියෙන සියලුම command files load කරගැනීම
+const pluginsPath = path.join(__dirname, 'plugins');
+
+if (fs.existsSync(pluginsPath)) {
+    const pluginFiles = fs.readdirSync(pluginsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of pluginFiles) {
+        const command = require(path.join(pluginsPath, file));
+        if (command.name) {
+            commands[command.name] = command;
+        }
+    }
 }
 
-// Default Commands එකතු කිරීම
-addCommand('ping', 'Bot වැඩ කරන්නේදැයි පරීක්ෂා කිරීමට', 'general', async (sock, remoteJid, msg) => {
-    await sock.sendMessage(remoteJid, { text: '🏓 Pong! SACHIYA MINI BOT සක්‍රීයයි.' }, { quoted: msg });
-});
-
-addCommand('owner', 'Owner ගේ තොරතුරු ලබා ගැනීමට', 'general', async (sock, remoteJid, msg) => {
-    await sock.sendMessage(remoteJid, { text: `👑 **Owner Info**\n\n- **Bot Name:** ${config.botName}\n- **Owner Number:** +${config.ownerNumber}` }, { quoted: msg });
-});
-
-addCommand('menu', 'සියලුම Commands ලැයිස්තුව', 'general', async (sock, remoteJid, msg) => {
-    let menuText = `🤖 *${config.botName}* 🤖\n\n`;
-    menuText += `📱 *Owner Number:* +${config.ownerNumber}\n\n`;
-    menuText += `📌 *Available Commands:*\n`;
-    
-    for (let cmd in commands) {
-        menuText += `• *${config.prefix}${commands[cmd].name}* : ${commands[cmd].desc}\n`;
-    }
-
-    await sock.sendMessage(remoteJid, { text: menuText }, { quoted: msg });
-});
-
-module.exports = { commands, addCommand };
+module.exports = { commands };
