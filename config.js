d22e4d005b34bddb15a97ecc183d746ccfccc1c4
Sachira-Induwara <sachirainduwara72@gmail.com>
2026-07-25 09:37:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     ownerNumber: "94760579211",
     prefix: ".",
     // මෙන්න මෙතනට ඔයාගේ Session ID එක Paste කරන්න:
-    sessionId: process.env.SESSION_ID || "ඔයාගේ_SESSION_ID_එක_මෙතනට_PASTE_කරන්න" 
+    sessionId: process.env.SESSION_ID || "ERZiQAzB#632IQC3NTr6cAJ7e-ccZgU2xGiPxOgefFTdLKs1Sv4o" 
 };
